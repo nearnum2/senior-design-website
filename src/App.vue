0055997e8e915f4581efffe4f48f7cd771bf7128
nearnum2 <87588542+nearnum2@users.noreply.github.com>
2022-07-29 17:08:08 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Dash_board/>
+    <v-main  class="mx-4" >
+      <router-view></router-view>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+// using: https://www.youtube.com/watch?v=BIuQfQcptIU&list=PL4cUxeGkcC9g0MQZfHwKcuB0Yswgb3gA5&index=4&ab_channel=TheNetNinja
+import Dash_board from '@/components/Dash_board.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dash_board, 
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
